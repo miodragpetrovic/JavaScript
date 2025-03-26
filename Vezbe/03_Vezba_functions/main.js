e59458338387d41a628cc7190f7f91a4c9dc25a4
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-03-26 14:56:15 +0100

    for

--- Vezbe/03_Vezba_functions/main.js
+++ Vezbe/03_Vezba_functions/main.js
@@ -1,6 +1,47 @@
-function showName(getUserName) {
-    console.log(getUserName);
+
+
+// Objekat
+const firstAndLastName = [
+
+        { firstName: "Denis", lastName: "Simeunovic", age: "59" },
+        { firstName: "Jelena", lastName: "Paunovic", age: "65" },
+        { firstName: "Ivana", lastName: "Dimitrijevic", age: "25" },
+        { firstName: "Dragan", lastName: "Radenovic", age: "41" },
+        { firstName: "Marina", lastName: "Prvulovic", age: "18" },
+        { firstName: "Miroljub", lastName: "Golubovic", age: "21" },
+        { firstName: "Nikola", lastName: "Simic", age: "33" },
+        { firstName: "Ivan", lastName: "Jovanovic", age: "28" },
+        { firstName: "Djuro", lastName: "Ivankovic", age: "29" },  
+];
+
+
+
+/* // Iscitavanje niza kroz index
+const names = ['Miodrag'];
+
+function showName(getFirstName, getLastName) {
+    return getFirstName + " " + getLastName;
 }
 
-showName("Miodrag");
-showName("Dejan");
+document.querySelector('.firstname').textContent = showName(firstAndLastName[0].firstName,firstAndLastName[0].lastName); */
+
+
+
+
+
+// Iscitavanje podataka pomocu petlje FOR
+
+function showName(getFirstName, getLastName, getAge) {
+    return getFirstName + " " + getLastName + " " + getAge;
+}
+
+for (let i = 0; i < firstAndLastName.length; i++) {
+    let p = document.createElement('p'); // kreiranje novog p elementa u HTML-u
+    let petlja = document.querySelector('.petlja')
+    p.textContent += showName(firstAndLastName[i].firstName, firstAndLastName[i].lastName, firstAndLastName[i].age);
+    petlja.appendChild(p);
+
+}
+
+
+
